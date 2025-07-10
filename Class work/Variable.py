b6eb18e8d6784c9ac687ca1de59fde85243f187b
sunitha-11 <sunithaeerla23@gmail.com>
2025-07-10 19:04:23 +0530

    first program

---
+++ Class work/Variable.py
@@ -0,0 +1,25 @@
+#basic variable assignments
+product_name="Laptop"
+price=45000
+in_stock =True
+print(product_name,price,in_stock)#outtput="Laptop 45000 True"
+
+#Multipleassignment
+a,b,c=10,20,30
+print(a,b,c)#output=10 20 30
+
+
+
+#Reassignments
+name ="sunitha"
+name ="sweety"
+print(name) #output=sweety
+
+
+#Sapping variables
+a,b=10,20
+a,b=b,a
+print(a,b) #output=20 10
+
+
+
